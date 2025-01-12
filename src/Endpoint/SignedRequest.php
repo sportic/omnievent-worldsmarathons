687ac0c47224556981b957112a5f7d4953845aa0
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-01-12 16:46:22 +0200

    feat: add serialiazable to SignedRequest

--- src/Endpoint/SignedRequest.php
+++ src/Endpoint/SignedRequest.php
@@ -2,8 +2,13 @@
 
 namespace Sportic\OmniEvent\Worldsmarathons\Endpoint;
 
-class SignedRequest
+use JsonSerializable;
+use Nip\Utility\Json\LoadFromJson;
+
+class SignedRequest implements JsonSerializable, \Serializable
 {
+    use LoadFromJson;
+
     public ?string $payload = null;
     public ?string $signature = null;
 
@@ -11,4 +16,36 @@ class SignedRequest
 
     public ?string $timestamp = null;
 
+    public function jsonSerialize()
+    {
+        return $this->serialize();
+    }
+
+    public function serialize(): ?string
+    {
+        return serialize($this->__serialize());
+    }
+
+    public function unserialize(string $data): void
+    {
+        $this->__unserialize(unserialize($data));
+    }
+
+    public function __serialize(): array
+    {
+        return [
+            'payload' => $this->payload,
+            'signature' => $this->signature,
+            'secret' => $this->secret,
+            'timestamp' => $this->timestamp,
+        ];
+    }
+
+    public function __unserialize($data): void
+    {
+        $this->payload = $data['payload'] ?? null;
+        $this->signature = $data['signature'] ?? null;
+        $this->secret = $data['secret'] ?? null;
+        $this->timestamp = $data['timestamp'] ?? null;
+    }
 }
